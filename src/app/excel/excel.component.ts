93d81e5b795fcba89d395eb2cf95c9c1ea71e60c
naomi68ha <naomi68ha@gmail.com>
2022-04-12 19:22:56 -0500

    firts

---
+++ src/app/excel/excel.component.ts
@@ -0,0 +1,75 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as XLSX from 'xlsx';
+
+@Component({
+  selector: 'app-excel',
+  templateUrl: './excel.component.html',
+  styleUrls: ['./excel.component.css']
+})
+export class ExcelComponent implements OnInit {
+
+  spinnerEnabled = false;
+  keys: string[] = [];
+  dataSheet: any;
+  @ViewChild('inputFile') inputFile!: ElementRef;
+  isExcelFile: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  onChange(evt: any) {
+    let data: any, header;
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    this.isExcelFile = !!target.files[0].name.match(/(.xls|.xlsx)/);
+    if (target.files.length > 1) {
+      this.inputFile.nativeElement.value = '';
+    }
+    if (this.isExcelFile) {
+      this.spinnerEnabled = true;
+      const reader: FileReader = new FileReader();
+      reader.onload = (e: any) => {
+        /* read workbook */
+        const bstr: string = e.target.result;
+        const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+        /* grab first sheet */
+        const wsname: string = wb.SheetNames[0];
+        const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+        /* save data */
+        data = XLSX.utils.sheet_to_json(ws);
+        this.dataSheet = data;
+
+        for (var val of data) {
+          let param: any = {
+            full_name: val['Nombre'],
+            phone: val['Número'],
+            email : val['Correo'],
+            url_linkedin : val['URL LinkedIn'],
+            work_position :val['Puesto'],
+            info_type : 1 
+          }
+        }
+      };
+
+      reader.readAsBinaryString(target.files[0]);
+
+      reader.onloadend = (e) => {
+        this.spinnerEnabled = false;
+        this.keys = Object.keys(data[0]);
+        this.dataSheet.next(data)
+      }
+    } else {
+      this.inputFile.nativeElement.value = '';
+    }
+  }
+
+  removeData() {
+    this.inputFile.nativeElement.value = '';
+    this.dataSheet.next(null);
+    this.keys = [];
+  }
+
+}
